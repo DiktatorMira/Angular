72764b2edbd071b7de74e6aaeb768967a96d6f8a
Diktator <dychervony@gmail.com>
2024-04-04 12:17:34 +0300

    Удалил лишние файлы в дз за 03.04.2024

--- Dz03.04.2024/src/app/app.module.ts
+++ Dz03.04.2024/src/app/app.module.ts
@@ -4,11 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
-import { PasswordMatchDirective } from './password-match.directive';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ AppComponent, FormComponent, PasswordMatchDirective ],
+  declarations: [ AppComponent, FormComponent ],
   imports: [ BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule ],
   providers: [ provideClientHydration() ],
   bootstrap: [AppComponent]
